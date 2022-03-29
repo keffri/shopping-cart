d199820606da1de14cd707c824cd97eda659b0eb
keffri <keffri.w.neal@gmail.com>
2022-03-28 18:20:33 -0700

    imported Cart

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import Cart from "./components/Cart";
+
 function App() {
   return (
-    <div className="app">
-      <h1>React Shopping Cart</h1>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
