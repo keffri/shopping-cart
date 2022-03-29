29aeb414add13a1ee1e3100639fec5cbc6409481
keffri <keffri.w.neal@gmail.com>
2022-03-28 18:21:30 -0700

    imported app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/reset.css";
 import "./styles/index.css";
-import RouteSwitch from "./RouteSwitch";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <RouteSwitch />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
