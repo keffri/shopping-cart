f0bb03e46e0b8161490e917435c27a9592d3b626
keffri <keffri.w.neal@gmail.com>
2022-04-10 18:25:03 -0700

    added about

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Shop from "./components/Shop";
+import About from "./components/About";
 import Cart from "./components/Cart";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
+          <Route path="/about" element={<About />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </div>
