4a3fa5364ef3ef902f587adca66030579e3fea5e
keffri <keffri.w.neal@gmail.com>
2022-04-12 18:07:38 -0700

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import video from "../videos/eldenRingBG.mp4";
 
 const Home = () => {
   return (
     <div className="home">
-      <h1>Home</h1>
+      <h1 className="home__title">The fallen leaves tell a story...</h1>
+      <video className="home__video" autoPlay muted loop>
+        <source src={video} type="video/mp4" />
+      </video>
     </div>
   );
 };
