bd3b7aa308d84feb6a179eb201815c5b0e802298
keffri <keffri.w.neal@gmail.com>
2022-04-19 19:17:03 -0700

    rune icon addition

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,12 +1,16 @@
 import React from "react";
+import rune_icon from "../images/rune_icon.png";
 
 const ItemCard = (props) => {
   return (
-    <div className="card">
+    <div className="card" onClick={props.onClick}>
       <img src={props.data.img} alt="img" className="item__image" />
       <div className="item__info">
         <p className="item__name">{props.data.name}</p>
-        <p className="item__price">{props.data.price} Runes</p>
+        <div className="item--inline">
+          <p className="item__price">{props.data.price}</p>
+          <img src={rune_icon} alt="rune" />
+        </div>
         <p className="item__description">{props.data.description}</p>
       </div>
     </div>
