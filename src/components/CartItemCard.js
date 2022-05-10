dd32437aa2ed6337203d002ae1a07e4d1776d64a
keffri <keffri.w.neal@gmail.com>
2022-05-09 17:17:50 -0700

    CartItemCard created

---
+++ src/components/CartItemCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import rune_icon from "../images/rune_icon.png";
+
+const CartItemCard = (props) => {
+  return (
+    <div className="card" onClick={props.onClick}>
+      <img src={props.data.img} alt="img" className="item__image" />
+      <div className="item__info">
+        <p className="item__name">{props.data.name}</p>
+        <div className="item--inline">
+          <p className="item__price">{props.data.price}</p>
+          <img src={rune_icon} alt="rune" />
+        </div>
+        <p className="item__description">{props.data.description}</p>
+      </div>
+      <button className="item__remove">X</button>
+    </div>
+  );
+};
+
+export default CartItemCard;
